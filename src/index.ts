3e275be36a923dd3250b44273ead91310bc205ef
ToastedToast <hey@toasted.dev>
2024-01-08 18:27:26 +0800

    feat(cli): init basic cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import "isomorphic-unfetch";
+
+import { intro, outro, text } from "@clack/prompts";
+
+const main = async () => {
+  intro("gitrover");
+  await text({
+    message: "What is your name?",
+    placeholder: "ToastedToast",
+  });
+  outro("You're all set!");
+};
+
+main();
