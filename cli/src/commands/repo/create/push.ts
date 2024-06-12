702874db5d536d4db24b88547fe119eaf309102d
ToastedToast <hey@toasted.dev>
2024-06-12 12:48:08 +0800

    feat(pushing): add select repo or init repo option

--- cli/src/commands/repo/create/push.ts
+++ cli/src/commands/repo/create/push.ts
@@ -1,4 +1,4 @@
-import { confirm, input } from "@inquirer/prompts";
+import { confirm, input, select } from "@inquirer/prompts";
 import type { Octokit } from "@octokit/rest";
 import chalk from "chalk";
 import {
@@ -12,13 +12,55 @@ import { askForRepoInfo } from "./index.js";
 import { handler } from "~/utils/command.js";
 
 export const createAndPushHandler = handler(async (octokit: Octokit) => {
-  // TODO: allow the user to select where the repo is if the current directory isn't a git repo
+  let cwd = undefined;
+
   if (!isGitRepository()) {
-    error(
-      "You are not in a git repository.\nInitialize a git repository with",
-      chalk.bold("git init") + ".",
-    );
-    process.exit(1);
+    const option = await select({
+      message:
+        "The current directory is not a git repository. What do you want to do?",
+      choices: [
+        { name: "Select my own directory", value: "select" },
+        { name: "Initialize a new git repository", value: "init" },
+        { name: "Exit", value: "exit" },
+      ],
+    });
+    switch (option) {
+      case "select":
+        {
+          const directory = await input({
+            message: "What directory is your repository located in?",
+            validate: (value) => {
+              if (value === "/" || value === ".")
+                return "Cannot be the current directory";
+              return value;
+            },
+          });
+
+          if (!isGitRepository(directory)) {
+            error(
+              "The repository you specified is not a git repository.\nInitialize a git repository with",
+              chalk.bold("git init") + ".",
+            );
+            process.exit(1);
+          }
+
+          cwd = directory;
+        }
+        break;
+      case "init":
+        {
+          execGitCommandSync(["init"], { stdio: "ignore" });
+          success("Initialized a new git repository.");
+        }
+        break;
+      case "exit": {
+        error(
+          "You are not in a git repository.\nInitialize a git repository with",
+          chalk.bold("git init") + ".",
+        );
+        process.exit(1);
+      }
+    }
   }
 
   const { owner, name, visibility, inOrg } = (await askForRepoInfo(octokit))!;
@@ -63,16 +105,15 @@ export const createAndPushHandler = handler(async (octokit: Octokit) => {
     });
   }
 
-  execGitCommandSync([
-    "remote",
-    "add",
-    remoteName,
-    await remoteUrl(owner, name),
-  ]);
+  execGitCommandSync(
+    ["remote", "add", remoteName, await remoteUrl(owner, name)],
+    { cwd },
+  );
 
   // TODO: check if repo has commits, and don't push if no commits
   execGitCommandSync(["push", "--set-upstream", remoteName, "HEAD"], {
     stdio: "ignore",
+    cwd,
   });
   success(`Pushed commits to repository ${chalk.bold(`${owner}/${name}`)}.`);
 });
--- cli/src/utils/git.ts
+++ cli/src/utils/git.ts
@@ -2,6 +2,7 @@ import type { ExecSyncOptions } from "child_process";
 import { exec, execSync } from "child_process";
 import { existsSync } from "fs";
 import { getUserConfig } from "./user-config.js";
+import { join } from "path";
 
 export const execGitCommandSync = (args: string[], options?: ExecSyncOptions) =>
   execSync("git " + args.join(" "), options);
@@ -11,9 +12,12 @@ export const execGitCommand = (
   callback?: Parameters<typeof exec>[1],
 ) => exec("git " + args.join(" "), callback);
 
-export const isGitRepository = () => existsSync(".git");
-export const gitRepoHasOrigin = () =>
-  execGitCommandSync(["remote", "show"], { encoding: "utf8" }).length !== 0;
+export const isGitRepository = (cwd?: string) =>
+  existsSync(join(cwd ?? ".", ".git"));
+
+export const gitRepoHasOrigin = (cwd?: string) =>
+  execGitCommandSync(["remote", "show"], { encoding: "utf8", cwd }).length !==
+  0;
 
 export const remoteUrl = async (repoOwner: string, repoName: string) => {
   const config = await getUserConfig();
