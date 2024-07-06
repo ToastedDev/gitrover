00fe10f0e3e6449b5c0101ae83f2d7c7029cf6d2
ToastedToast <hey@toasted.dev>
2024-07-06 20:59:51 +0800

    update links, improve colors

--- web/app/layout.config.tsx
+++ web/app/layout.config.tsx
@@ -1,18 +1,20 @@
 import { type BaseLayoutProps, type DocsLayoutProps } from "fumadocs-ui/layout";
 import { pageTree } from "@/app/source";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
 
 // shared configuration
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: "https://github.com/ToastedDev/gitrover",
   nav: {
-    title: "My App",
+    title: (
+      <div className="flex items-center gap-1.5">
+        <Image src={Logo} alt="GitRover Logo" width={24} height={24} />
+        <span className="font-medium max-md:[header_&]:hidden">GitRover</span>
+      </div>
+    ),
   },
-  links: [
-    {
-      text: "Documentation",
-      url: "/docs",
-      active: "nested-url",
-    },
-  ],
+  links: [],
 };
 
 // docs layout configuration
